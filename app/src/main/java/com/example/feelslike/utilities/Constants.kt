b29e11b718808f173cbcd85ddee4ffe3528e4111
Emily Thorne <53380331+sssaucie@users.noreply.github.com>
2021-05-20 10:09:13 -0600

    Firebase update
    updated SDK-1 auth in google-properties file, updated name with app json file

--- app/src/main/java/com/example/feelslike/utilities/Constants.kt
+++ app/src/main/java/com/example/feelslike/utilities/Constants.kt
@@ -1,13 +1,11 @@
 package com.example.feelslike.utilities
 
-import com.example.feelslike.model.entity.FeelsLikeEntity
-
 /**
  * Constants used throughout the app
  */
 
 const val DATABASE_NAME = "feels_like_db"
-const val FEELS_LIKE_DATA_FILENAME = "feels_like.json"
+const val FEELS_LIKE_DATA_FILENAME = "feelsLike.json"
 
 const val FIRST_RUN_KEY = "first_run"
 const val SHARED_PREFS_KEY = "shared_prefs"
