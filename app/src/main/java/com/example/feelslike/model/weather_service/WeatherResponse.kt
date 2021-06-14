a00b7d47607c1d1a57b715eedc73dd52abf129cc
Emily Thorne <53380331+sssaucie@users.noreply.github.com>
2021-06-13 21:51:42 -0600

    weatherresponse
    added Parcelable implementation to WeatherResponse

--- app/src/main/java/com/example/feelslike/model/weather_service/WeatherResponse.kt
+++ app/src/main/java/com/example/feelslike/model/weather_service/WeatherResponse.kt
@@ -1,22 +1,35 @@
 package com.example.feelslike.model.weather_service
 
+import android.os.Parcelable
+import com.google.android.gms.maps.model.LatLng
+import kotlinx.parcelize.IgnoredOnParcel
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
 data class WeatherResponse(
     val coord : Coordinates,
     val weather : List<WeatherParams>,
     val main : BaseWeatherStats
-)
+) : Parcelable
 {
+    @Parcelize
     data class Coordinates(
         val lon : Double,
         val lat : Double
-    )
+    ) : Parcelable
+    {
+        val hasCoordinates
+            get() = LatLng(lat, lon)
+    }
 
+    @Parcelize
     data class WeatherParams(
         val id : Int,
         val main : String,
-        val description : String,
-    )
+        val description : String
+    ) : Parcelable
 
+    @Parcelize
     data class BaseWeatherStats(
         val temp : Float,
         val feels_like : Float,
@@ -24,5 +37,5 @@ data class WeatherResponse(
         val temp_max : Double,
         val pressure : Int,
         val humidity : Int
-    )
+    ) : Parcelable
 }
